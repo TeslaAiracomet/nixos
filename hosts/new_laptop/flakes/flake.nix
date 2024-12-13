e113542ec9c068853d345f5c68df8b3395863a7e
Areg Zaratsyan <masterareg@hotmail.com>
2024-12-13 01:08:21 -0500

    file structure for nixos and new laptop config

---
+++ hosts/new_laptop/flakes/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... } @ inputs: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    homeConfiguration."tesla" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+
+      modules = [ ./home.nix ];
+    };
+
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs system; };
+      modules = [
+        ./configuration.nix
+	inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
